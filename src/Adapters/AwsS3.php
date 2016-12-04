64ac1845c7b741772fbd0095920f2184f134098b
Brian Faust <hello@brianfaust.de>
2016-12-04 10:04:02 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Adapters/AwsS3.php
+++ src/Adapters/AwsS3.php
@@ -11,6 +11,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of Laravel Picible.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace BrianFaust\Picible\Adapters;
 
 use Aws\S3\S3Client;
